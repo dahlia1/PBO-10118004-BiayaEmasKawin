d19d312a56b493cf3772488711c05211eff4222d
dahlia1 <55682672+dahlia1@users.noreply.github.com>
2019-10-17 20:46:48 +0700

    Add files via upload

---
+++ PBO-10118004-BiayaEmasKawin/PBO-10118004-BiayaemasKawin/src/pbo/pkg10118004/biayaemaskawin/PBO10118004BiayaemasKawin.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118004.biayaemaskawin;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ *
+ * @author ASUS A411UF
+ */
+public class PBO10118004BiayaemasKawin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         DecimalFormat mataUangIndonesia = (DecimalFormat) 
+                      DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRP = new DecimalFormatSymbols();
+        
+        formatRP.setCurrencySymbol("Rp.");
+        formatRP.setMonetaryDecimalSeparator(',');
+        formatRP.setGroupingSeparator('.');
+        
+        mataUangIndonesia.setDecimalFormatSymbols(formatRP);
+        
+        
+        emas objEmas = new emas();
+        objEmas.setHargaGram(570000);
+        objEmas.setBeratEmas(15.7);
+        System.out.println("====Aplikasi Hitung Harga Emas====");
+        System.out.printf("Total Bayar Emas : %s %n ", 
+                           mataUangIndonesia.format(
+                           objEmas.perhitungan(objEmas.getHargaGram(), 
+                           objEmas.getBeratEmas())));
+
+    }
+    
+}
+
+    
